5752bb7d150281306c0f072071e9d1275d2b5682
Dave Coleman <dave@dvclmn.com>
2025-05-03 07:54:56 +0200

    Re-looking at the general code here

--- Sources/LilyPad/ExampleView.swift
+++ Sources/LilyPad/ExampleView.swift
@@ -7,27 +7,27 @@
 
 import SwiftUI
 
-//
-//struct GestureExampleView: View {
-//  
-////  @State private var gestureValues: GestureValues = .init()
-//  
-//  public init() {
-//  }
-//  
-//  var body: some View {
-//    
-//    Text("Hello")
-//      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-//      .trackpadGestures()
-//      .background(.black.opacity(0.8))
-//      .background(.purple.opacity(0.3))
-//    
-//  }
-//}
-//#if DEBUG
-//#Preview(traits: .fixedLayout(width: 500, height: 700)) {
-//  GestureExampleView()
-//}
-//#endif
-//
+
+struct GestureExampleView: View {
+  
+//  @State private var gestureValues: GestureValues = .init()
+  
+  public init() {
+  }
+  
+  var body: some View {
+    
+    Text("Hello")
+      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+      .trackpadGestures()
+      .background(.black.opacity(0.8))
+      .background(.purple.opacity(0.3))
+    
+  }
+}
+#if DEBUG
+#Preview(traits: .fixedLayout(width: 500, height: 700)) {
+  GestureExampleView()
+}
+#endif
+
