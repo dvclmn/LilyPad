cc2b58dabaa58dc51fa0753aeb93ca129329da5c
Dave Coleman <dave@dvclmn.com>
2025-05-04 14:37:17 +0200

    Attempting to improve varying-width strokes

--- Sources/LilyPad/FingerPaint/CanvasView.swift
+++ Sources/LilyPad/FingerPaint/CanvasView.swift
@@ -19,25 +19,33 @@ public struct CanvasView: View {
 
       TouchIndicatorsView(handler: handler)
 
-      Canvas { context, _ in
-
+      Canvas {
+        context,
+        _ in
+        
         /// Draw all strokes
         for stroke in handler.strokeHandler.allStrokes {
           let path = StrokePath.smoothPath(for: stroke)
-
-          // Draw the stroke with variable width
+          
+          context.withCGContext(content: <#T##(CGContext) throws -> Void#>)
+          
+          /// Draw the stroke with variable width
           for i in 0..<stroke.points.count {
             if i < stroke.points.count - 1 {
-              // Create segment path between points
+              /// Create segment path between points
               var segmentPath = Path()
               segmentPath.move(to: stroke.points[i])
               segmentPath.addLine(to: stroke.points[i+1])
               
-              // Average width between adjacent points
+              /// Average width between adjacent points
               let width = (stroke.widths[i] + stroke.widths[min(i+1, stroke.widths.count-1)]) / 2.0
               
-              // Draw the segment with the calculated width
-              context.stroke(segmentPath, with: .color(stroke.color), style: StrokeStyle(lineWidth: width, lineCap: .round, lineJoin: .round))
+              /// Draw the segment with the calculated width
+              context.stroke(
+                segmentPath,
+                with: .color(stroke.color),
+                style: StrokeStyle(lineWidth: width, lineCap: .round, lineJoin: .round)
+              )
             }
           }
           /// Shows location of points and handles
