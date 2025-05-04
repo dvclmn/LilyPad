8a916a0eef4bf9f9f91e3e62983bf77e6a68710b
Dave Coleman <dave@dvclmn.com>
2025-05-04 14:08:24 +0200

    Updates

--- Sources/LilyPad/Archive/TrackpadTouch.swift
+++ Sources/LilyPad/Archive/TrackpadTouch.swift
@@ -5,116 +5,116 @@
 //  Created by Dave Coleman on 18/11/2024.
 //
 
-#if canImport(AppKit)
-import SwiftUI
-import AppKit
-
-public protocol AppKitTouchesViewDelegate: AnyObject {
-  /// Provides `.touching` touches only.
-  func touchesView(_ view: AppKitTouchesView, didUpdateTouchingTouches touches: Set<NSTouch>)
-}
-
-public final class AppKitTouchesView: NSView {
-  weak var delegate: AppKitTouchesViewDelegate?
-  
-  public override init(frame frameRect: NSRect) {
-    super.init(frame: frameRect)
-    /// We're interested in `.indirect` touches only.
-    allowedTouchTypes = [.indirect]
-    /// We'd like to receive resting touches as well.
-    wantsRestingTouches = true
-  }
-  
-  public required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  private func handleTouches(with event: NSEvent) {
-    // Get all `.touching` touches only (includes `.began`, `.moved` & `.stationary`).
-    let touches = event.touches(matching: .touching, in: self)
-    // Forward them via delegate.
-    delegate?.touchesView(self, didUpdateTouchingTouches: touches)
-  }
-  
-  public override func touchesBegan(with event: NSEvent) {
-    handleTouches(with: event)
-  }
-  
-  public override func touchesEnded(with event: NSEvent) {
-    handleTouches(with: event)
-  }
-  
-  public override func touchesMoved(with event: NSEvent) {
-    handleTouches(with: event)
-  }
-  
-  public override func touchesCancelled(with event: NSEvent) {
-    handleTouches(with: event)
-  }
-}
-
-public struct NSTrackPadTouch: Identifiable {
-
-  public var id: Int
-  
-  /// Normalized touch X position on a device (0.0 - 1.0).
-  public let normalizedX: CGFloat
-  
-  /// Normalized touch Y position on a device (0.0 - 1.0).
-  public let normalizedY: CGFloat
-  
-  public init(_ nsTouch: NSTouch) {
-    self.normalizedX = nsTouch.normalizedPosition.x
-    /// `NSTouch.normalizedPosition.y` is flipped -> 0.0 means bottom. But the
-    /// `Touch` structure is meants to be used with the SwiftUI -> flip it.
-    self.normalizedY = 1.0 - nsTouch.normalizedPosition.y
-    self.id = nsTouch.hash
-  }
-  
-  public init(x: CGFloat, y: CGFloat) {
-    self.id = UUID().hashValue
-    self.normalizedX = x
-    self.normalizedY = 1.0 - y
-  }
-}
-
-public struct TouchesView: NSViewRepresentable {
-  
-  /// Up to date list of touching touches.
-  @Binding public var touches: [TrackPadTouch]
-  
-  public init(
-    touches: Binding<[TrackPadTouch]>
-  ) {
-    self._touches = touches
-  }
-  
-  public func updateNSView(_ nsView: AppKitTouchesView, context: Context) {
-  }
-      
-  public func makeNSView(context: Context) -> AppKitTouchesView {
-    let view = AppKitTouchesView()
-    view.delegate = context.coordinator
-    return view
-  }
-  
-  public func makeCoordinator() -> Coordinator {
-    Coordinator(self)
-  }
-  
-  public class Coordinator: NSObject, AppKitTouchesViewDelegate {
-    let parent: TouchesView
-    
-    public init(_ view: TouchesView) {
-      self.parent = view
-    }
-    
-    public func touchesView(_ view: AppKitTouchesView, didUpdateTouchingTouches touches: Set<NSTouch>) {
-      parent.touches = touches.map(TrackPadTouch.init)
-    }
-  }
-}
-#endif
+//#if canImport(AppKit)
+//import SwiftUI
+//import AppKit
+//
+//public protocol AppKitTouchesViewDelegate: AnyObject {
+//  /// Provides `.touching` touches only.
+//  func touchesView(_ view: AppKitTouchesView, didUpdateTouchingTouches touches: Set<NSTouch>)
+//}
+//
+//public final class AppKitTouchesView: NSView {
+//  weak var delegate: AppKitTouchesViewDelegate?
+//  
+//  public override init(frame frameRect: NSRect) {
+//    super.init(frame: frameRect)
+//    /// We're interested in `.indirect` touches only.
+//    allowedTouchTypes = [.indirect]
+//    /// We'd like to receive resting touches as well.
+//    wantsRestingTouches = true
+//  }
+//  
+//  public required init?(coder: NSCoder) {
+//    fatalError("init(coder:) has not been implemented")
+//  }
+//  
+//  private func handleTouches(with event: NSEvent) {
+//    // Get all `.touching` touches only (includes `.began`, `.moved` & `.stationary`).
+//    let touches = event.touches(matching: .touching, in: self)
+//    // Forward them via delegate.
+//    delegate?.touchesView(self, didUpdateTouchingTouches: touches)
+//  }
+//  
+//  public override func touchesBegan(with event: NSEvent) {
+//    handleTouches(with: event)
+//  }
+//  
+//  public override func touchesEnded(with event: NSEvent) {
+//    handleTouches(with: event)
+//  }
+//  
+//  public override func touchesMoved(with event: NSEvent) {
+//    handleTouches(with: event)
+//  }
+//  
+//  public override func touchesCancelled(with event: NSEvent) {
+//    handleTouches(with: event)
+//  }
+//}
+//
+//public struct NSTrackPadTouch: Identifiable {
+//
+//  public var id: Int
+//  
+//  /// Normalized touch X position on a device (0.0 - 1.0).
+//  public let normalizedX: CGFloat
+//  
+//  /// Normalized touch Y position on a device (0.0 - 1.0).
+//  public let normalizedY: CGFloat
+//  
+//  public init(_ nsTouch: NSTouch) {
+//    self.normalizedX = nsTouch.normalizedPosition.x
+//    /// `NSTouch.normalizedPosition.y` is flipped -> 0.0 means bottom. But the
+//    /// `Touch` structure is meants to be used with the SwiftUI -> flip it.
+//    self.normalizedY = 1.0 - nsTouch.normalizedPosition.y
+//    self.id = nsTouch.hash
+//  }
+//  
+//  public init(x: CGFloat, y: CGFloat) {
+//    self.id = UUID().hashValue
+//    self.normalizedX = x
+//    self.normalizedY = 1.0 - y
+//  }
+//}
+//
+//public struct TouchesView: NSViewRepresentable {
+//  
+//  /// Up to date list of touching touches.
+//  @Binding public var touches: [TrackPadTouch]
+//  
+//  public init(
+//    touches: Binding<[TrackPadTouch]>
+//  ) {
+//    self._touches = touches
+//  }
+//  
+//  public func updateNSView(_ nsView: AppKitTouchesView, context: Context) {
+//  }
+//      
+//  public func makeNSView(context: Context) -> AppKitTouchesView {
+//    let view = AppKitTouchesView()
+//    view.delegate = context.coordinator
+//    return view
+//  }
+//  
+//  public func makeCoordinator() -> Coordinator {
+//    Coordinator(self)
+//  }
+//  
+//  public class Coordinator: NSObject, AppKitTouchesViewDelegate {
+//    let parent: TouchesView
+//    
+//    public init(_ view: TouchesView) {
+//      self.parent = view
+//    }
+//    
+//    public func touchesView(_ view: AppKitTouchesView, didUpdateTouchingTouches touches: Set<NSTouch>) {
+//      parent.touches = touches.map(TrackPadTouch.init)
+//    }
+//  }
+//}
+//#endif
 
 //
 //struct ExampleTrackpadView: View {
--- Sources/LilyPad/FingerPaint/CanvasView.swift
+++ Sources/LilyPad/FingerPaint/CanvasView.swift
@@ -24,16 +24,32 @@ public struct CanvasView: View {
         /// Draw all strokes
         for stroke in handler.strokeHandler.allStrokes {
           let path = StrokePath.smoothPath(for: stroke)
-
-          context.stroke(
-            path,
-            with: .color(stroke.color),
-            style: StrokeStyle(
-              lineWidth: 5,
-              lineCap: .round,
-              lineJoin: .round
-            )
-          )  // END context stroke
+          
+          /// Draw the stroke with variable width
+          for i in 0..<stroke.points.count {
+            if i < stroke.points.count - 1 {
+              /// Create segment path between points
+              var segmentPath = Path()
+              segmentPath.move(to: stroke.points[i])
+              segmentPath.addLine(to: stroke.points[i+1])
+              
+              /// Average width between adjacent points
+              let width = (stroke.widths[i] + stroke.widths[min(i+1, stroke.widths.count-1)]) / 2.0
+              
+              /// Draw the segment with the calculated width
+              context.stroke(segmentPath, with: .color(stroke.color), style: StrokeStyle(lineWidth: width, lineCap: .round, lineJoin: .round))
+            }
+          }
+//
+//          context.stroke(
+//            path,
+//            with: .color(stroke.color),
+//            style: StrokeStyle(
+//              lineWidth: 5,
+//              lineCap: .round,
+//              lineJoin: .round
+//            )
+//          )  // END context stroke
 
           /// Shows location of points and handles
           context.debugPath(path: path)
