658994fde80ed51a0b5857d82625ca0775dab93c
Dave Coleman <dave@dvclmn.com>
2025-05-05 20:34:01 +0200

    Removes RawTouches from TouchStroke...
    ... which may be useful to refer to later on

--- Sources/LilyPad/TrackpadTouches/Models/Model+Stroke.swift
+++ Sources/LilyPad/TrackpadTouches/Models/Model+Stroke.swift
@@ -8,24 +8,6 @@
 import MemberwiseInit
 import SwiftUI
 
-//public struct RawTouches: Identifiable, Equatable, Hashable {
-//  
-//  public typealias ID = TouchStroke.ID
-//  public let id: ID
-//  public let touches: [TrackpadTouch]
-//}
-
-//extension Array where Element == RawTouches {
-//  
-//  public func getTouchesForID(_ id: TouchStroke.ID) -> [TrackpadTouch] {
-//    let rawTouches = self.first { touch in
-//      touch.id == id
-//    }
-//    return rawTouches?.touches ?? []
-//  }
-//}
-
-
 /// Represents a touch stroke with a series of points and widths
 @MemberwiseInit(.public)
 public struct TouchStroke: Identifiable {
@@ -33,44 +15,21 @@ public struct TouchStroke: Identifiable {
   public var points: [StrokePoint]
   public var colour: Color
 
-  /// These are the *original* points recieved, before filtering.
-  /// Used for debugging only
-//  public var rawTouchPoints: [TrackpadTouch] = []
-
   /// Add a point to the stroke with a specified width
-  public mutating func addPoint(
-    _ point: StrokePoint,
-//    kind: StrokePointType
-  ) {
-//    switch kind {
-//      case .strokePoint(let newPoints):
-        points.append(point)
+  public mutating func addPoint(_ point: StrokePoint) {
+    points.append(point)
 
-//      case .rawTouchPoint(let newTouches):
-//        fatalError("Raw touches no longer supported here")
-//        rawTouchPoints.append(newTouches)
-//    }
   }
 
   public var cgPoints: [CGPoint] {
     return points.map(\.position)
   }
-  
-//  public mutating func clearRawTouches() {
-//    rawTouchPoints.removeAll()
-//  }
 
   public subscript(pointAtIndex index: Int) -> CGPoint {
     let result = points[index].position
     return result
   }
 }
-
-
-//public enum StrokePointType {
-//  case strokePoint(StrokePoint)
-//  case rawTouchPoint(TrackpadTouch)
-//}
 
 enum StrokeState {
   case active
