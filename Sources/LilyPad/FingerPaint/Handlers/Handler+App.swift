0a44980616a5ca22746d57eb8aa14c685a261403
Dave Coleman <dave@dvclmn.com>
2025-05-05 07:53:00 +0200

    Removes unused AppHandler>process touches...
    ... method, which may be useful to refer to later

--- Sources/LilyPad/FingerPaint/Handlers/Handler+App.swift
+++ Sources/LilyPad/FingerPaint/Handlers/Handler+App.swift
@@ -22,9 +22,6 @@ public final class AppHandler {
   var isPointerLocked: Bool = false
   var isClicked: Bool = false
 
-  //  let minDistanceThreshold: CGFloat = 10
-  //  let minSpeedForSparseSampling: CGFloat = 0.1
-
   public init() {}
 
 }
@@ -114,73 +111,9 @@ extension AppHandler {
     //    let timeElapsed = timeEnded - timeStarted
     //    print("Time taken to process touches: \(timeElapsed) seconds")
   }
-  //  public func processTouches() {
-  //
-  //    guard isInTouchMode else {
-  //      print("No need to process touches, not in touch mode.")
-  //      return
-  //    }
-  //
-  //    if isDebugMode { print("Processing touches. Touch count: \(touches.count)") }
-  //    /// Process each touch to update strokes
-  //    for touch in touches {
-  //      let touchId = touch.id
-  //      let touchPosition = touchPosition(touch)
-  //
-  //      /// Calculate width based on velocity
-  //      let width = strokeHandler.strokeWidth.calculateStrokeWidth(for: touch)
-  //
-  //      /// Update or create stroke for this touch
-  //      if var stroke = strokeHandler.activeStrokes[touchId] {
-  //        if isDebugMode {
-  //          print("There are active strokes for touch \(touchId). Count: \(stroke.points.count)")
-  //          print("Point count for stroke BEFORE: \(stroke.points.count)")
-  //        }
-  //
-  //        if let last = stroke.points.last {
-  //          let distance = hypot(touchPosition.x - last.x, touchPosition.y - last.y)
-  //          let shouldAddPoint = distance > minDistanceThreshold || touch.speed < minSpeedForSparseSampling
-  //          if shouldAddPoint {
-  //            stroke.addPoint(touchPosition, width: width)
-  //          }
-  //        }
-  //
-  //
-  ////        stroke.addPoint(touchPosition, width: width)
-  //        strokeHandler.activeStrokes[touchId] = stroke
-  //
-  //      } else {
-  //        /// New touch, create a new stroke
-  //        if isDebugMode { print("Creating a new stroke for touch \(touchId)") }
-  //
-  //        let stroke = TouchStroke(
-  //          points: [touchPosition],
-  //          widths: [2],
-  //          color: .purple
-  //        )
-  //        strokeHandler.activeStrokes[touchId] = stroke
-  //      }
-  //    }
-  //
-  //    /// Check for ended touches
-  //    let currentIds = Set(touches.map { $0.id })
-  //    let activeIds = Set(strokeHandler.activeStrokes.keys)
-  //    let endedIds = activeIds.subtracting(currentIds)
-  //
-  //    /// Finalize ended strokes
-  //    for touchId in endedIds {
-  //      if let stroke = strokeHandler.activeStrokes[touchId], stroke.points.count >= strokeHandler.minPointsForCurve {
-  //        strokeHandler.completedStrokes.append(stroke)
-  //      }
-  //      strokeHandler.activeStrokes.removeValue(forKey: touchId)
-  //    }
-  //  }
-
-  // MARK: - Strokes
-
+  
   public func clearStrokes() {
-    strokeHandler.activeStrokes.removeAll()
-    strokeHandler.completedStrokes.removeAll()
+    strokeHandler.clearStrokes()
   }
 
 
