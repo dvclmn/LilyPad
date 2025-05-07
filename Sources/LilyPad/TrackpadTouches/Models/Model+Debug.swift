d645325b1762fe61db068566c9b9ce0392d7882e
Dave Coleman <dave@dvclmn.com>
2025-05-07 11:20:49 +0200

    Updates to Debug item model

--- Sources/LilyPad/TrackpadTouches/Models/Model+Debug.swift
+++ Sources/LilyPad/TrackpadTouches/Models/Model+Debug.swift
@@ -5,29 +5,43 @@
 //  Created by Dave Coleman on 5/5/2025.
 //
 
-import Foundation
+import SwiftUI
+import BaseComponents
 
-public enum TouchDebugItem: String, Identifiable, CaseIterable {
-  case touchCount
-  case strokeCountActive
-  case strokeCountCompleted
-  case strokeCountTotal
-  case pointCountFiltered
-//  case pointCountRaw
+public enum TouchDebugItem: String, HBarItem {
+  case counts
+//  case strokeCountActive
+//  case strokeCountCompleted
+//  case strokeCountTotal
+//  case pointCountFiltered
   case touchModeActive
   case pointerLocked
   case clickedDown
   case pressure
   
+  public static let itemsKey: String = "InfoBarItems"
+  public static let defaultItems: Set<Self> = [
+    .counts,
+//    .strokeCountActive,
+//    .strokeCountCompleted,
+//    .strokeCountTotal,
+//    .pointCountFiltered,
+    .touchModeActive,
+    .pointerLocked,
+//    .clickedDown,
+    .pressure
+  ]
+  
   public var id: String { self.rawValue }
   
   public var name: String {
     switch self {
-      case .touchCount: "Touch Count"
-      case .strokeCountActive: "Strokes (Active)"
-      case .strokeCountCompleted: "Strokes (Completed)"
-      case .strokeCountTotal: "Strokes (Total)"
-      case .pointCountFiltered: "Points (Filtered)"
+      case .counts: "Counts"
+//      case .touchCount: "Touch Count"
+//      case .strokeCountActive: "Strokes (Active)"
+//      case .strokeCountCompleted: "Strokes (Completed)"
+//      case .strokeCountTotal: "Strokes (Total)"
+//      case .pointCountFiltered: "Points (Filtered)"
 //      case .pointCountRaw: "Points (Raw)"
       case .touchModeActive: "Touch Mode Active"
       case .pointerLocked: "Pointer Locked"
@@ -35,4 +49,29 @@ public enum TouchDebugItem: String, Identifiable, CaseIterable {
       case .pressure: "Pressure"
     }
   }
+  
+  public var label: QuickLabel {
+    return QuickLabel(self.name)
+  }
 }
+
+public enum DebugCounts: String, CaseIterable, Identifiable {
+  case touches
+  case points
+//  case pointsFiltered
+  case strokesActive
+  case strokesCompleted
+  
+  public var id: String { rawValue }
+  public var name: String {
+    switch self {
+      case .touches: "Touches"
+      case .points: "Points"
+//      case .pointsFiltered: "Points (Filtered)"
+      case .strokesActive: "Strokes (Active)"
+      case .strokesCompleted: "Touches (Completed)"
+    }
+  }
+  
+}
+
