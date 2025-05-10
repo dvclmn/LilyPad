bd4364f8e8fa8b4d79187c371285d6240b196ef2
Dave Coleman <dave@dvclmn.com>
2025-05-10 16:45:38 +0200

    Updates to gestures

--- Sources/LilyPad/TrackpadTouches/Handlers/Handler+Gesture.swift
+++ Sources/LilyPad/TrackpadTouches/Handlers/Handler+Gesture.swift
@@ -7,28 +7,40 @@
 
 import BaseHelpers
 import Foundation
+import SwiftUI
 
 public struct GestureStateHandler {
-
+  
   /// Current values
   public var pan: CGPoint = .zero
   public var zoom: CGFloat = 1.0
   public var rotation: CGFloat = .zero
-
+  
   /// Last-updated values
   var lastPan: CGPoint = .zero
   var lastZoom: CGFloat = 1.0
   var lastRotation: CGFloat = .zero
-
+  
   var startTouchPositions: TouchPositions?
+  var currentTouchPositions: TouchPositions?
+  
+  
   var gestureType: GestureType = .none
-
+  
   let zoomThreshold: CGFloat = 20
   let panThreshold: CGFloat = 10
   let rotationThreshold: CGFloat = .pi / 30  // ~6 degrees
   let requiredTouchCount: Int = 2
-
+  
   public init() {}
+}
+
+extension GestureStateHandler {
+  
+  public func currentTouchesMidpoint(in viewSize: CGSize) -> UnitPoint {
+    guard let currentTouchPositions else { return .center }
+    return currentTouchPositions.midPoint.unitPoint(in: viewSize)
+  }
 
   public mutating func update(event: TouchEventData, inViewSize rect: CGRect) {
 
@@ -38,6 +50,7 @@ public struct GestureStateHandler {
     switch event.phase {
       case .began:
         startTouchPositions = touchPositions
+        currentTouchPositions = touchPositions
         gestureType = .none
 
       case .moved:
@@ -45,33 +58,44 @@ public struct GestureStateHandler {
           print("Gesture: No value found for `startPositions`")
           return
         }
+        
+        currentTouchPositions = touchPositions
 
         let deltaPan = touchPositions.midPoint - start.midPoint
         let deltaZoom = abs(touchPositions.distanceBetween - start.distanceBetween)
         let deltaAngle = abs(touchPositions.angleBetween - start.angleBetween)
 
         /// Decide gesture type if undecided
+//        if gestureType == .none {
+//          if deltaZoom > zoomThreshold {
+//            print("Zoom delta: `\(deltaZoom)` crossed threshold `\(zoomThreshold)`")
+//            gestureType = .zoom
+//          } else if deltaAngle > rotationThreshold {
+//            print("Rotation delta: `\(deltaAngle)` crossed threshold `\(rotationThreshold)`")
+//            gestureType = .rotate
+//          } else if deltaPan.length > panThreshold {
+//            print("Zoom delta: `\(deltaZoom)` crossed threshold `\(zoomThreshold)`")
+//            gestureType = .pan
+//          }
+//        }
+        
         if gestureType == .none {
-          if deltaZoom > zoomThreshold {
-            print("Zoom delta: `\(deltaZoom)` crossed threshold `\(zoomThreshold)`")
-            gestureType = .zoom
-          } else if deltaAngle > rotationThreshold {
-            print("Rotation delta: `\(deltaAngle)` crossed threshold `\(rotationThreshold)`")
-            gestureType = .rotate
-          } else if deltaPan.length > panThreshold {
-            print("Zoom delta: `\(deltaZoom)` crossed threshold `\(zoomThreshold)`")
+          let zoomPassed = deltaZoom > zoomThreshold
+          let rotatePassed = deltaAngle > rotationThreshold
+          let panPassed = deltaPan.length > panThreshold
+          
+          /// Priority: pan > zoom > rotate
+          if panPassed {
             gestureType = .pan
+            print("Now Panning: Pan delta `\(deltaPan)` crossed threshold `\(panThreshold)`")
+          } else if zoomPassed {
+            gestureType = .zoom
+            print("Now Zooming: Zoom delta `\(deltaZoom)` crossed threshold `\(zoomThreshold)`")
+          } else if rotatePassed {
+            gestureType = .rotate
+            print("Now Rotating: Rotation delta `\(deltaAngle)` crossed threshold `\(rotationThreshold)`")
           }
         }
-
-        //        if deltaDistance > zoomThreshold {
-        //          let scaleChange = touchPositions.distanceBetween / start.distanceBetween
-        //
-        //          zoom = lastZoom * scaleChange
-        //        } else {
-        //          // Don't update scale if zoom motion is below threshold
-        //          zoom = lastZoom
-        //        }
 
         switch gestureType {
           case .zoom:
@@ -92,6 +116,7 @@ public struct GestureStateHandler {
         lastZoom = zoom
         lastRotation = rotation
         startTouchPositions = nil
+        currentTouchPositions = nil
         gestureType = .none
     }
 
