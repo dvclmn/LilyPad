b74cee8f439e3069d1e5de1c2d15d32be3af1554
Dave Coleman <dave@dvclmn.com>
2025-05-11 11:45:05 +0200

    Doing a tidy. Removes another swathe of now...
    ... deprecated Gesture code — especially from when Lilypad was more focused on native NSGestures, which is less-so the case now.
    - Also removes more recent ZoomView related code, which was recently incorporated directly into  Paperbark, so as to reduce some points of failure

--- Sources/LilyPad/TrackpadTouches/Handlers/Handler+Gesture.swift
+++ Sources/LilyPad/TrackpadTouches/Handlers/Handler+Gesture.swift
@@ -10,38 +10,38 @@ import Foundation
 import SwiftUI
 
 public struct GestureStateHandler {
-  
+
   /// Current values
   public var pan: CGPoint = .zero
   public var zoom: CGFloat = 1.0
   public var rotation: CGFloat = .zero
-  
+
   /// Last-updated values
   var lastPan: CGPoint = .zero
   var lastZoom: CGFloat = 1.0
   var lastRotation: CGFloat = .zero
-  
+
   var startTouchPositions: TouchPositions?
   var currentTouchPositions: TouchPositions?
-  
-  
+
+
   var gestureType: GestureType = .none
-  
+
   let zoomThreshold: CGFloat = 20
   let panThreshold: CGFloat = 10
   let rotationThreshold: CGFloat = .pi / 30  // ~6 degrees
   let requiredTouchCount: Int = 2
-  
+
   public init() {}
 }
 
 extension GestureStateHandler {
-  
+
   public func currentTouchesMidpoint(in viewSize: CGSize) -> UnitPoint {
     guard let currentTouchPositions else { return .center }
     return currentTouchPositions.midPoint.unitPoint(in: viewSize)
   }
-  
+
   public mutating func resetValue(for gestureType: GestureType) {
     switch gestureType {
       case .pan:
@@ -71,32 +71,18 @@ extension GestureStateHandler {
           print("Gesture: No value found for `startPositions`")
           return
         }
-        
+
         currentTouchPositions = touchPositions
 
         let deltaPan = touchPositions.midPoint - start.midPoint
         let deltaZoom = abs(touchPositions.distanceBetween - start.distanceBetween)
         let deltaAngle = abs(touchPositions.angleBetween - start.angleBetween)
 
-        /// Decide gesture type if undecided
-//        if gestureType == .none {
-//          if deltaZoom > zoomThreshold {
-//            print("Zoom delta: `\(deltaZoom)` crossed threshold `\(zoomThreshold)`")
-//            gestureType = .zoom
-//          } else if deltaAngle > rotationThreshold {
-//            print("Rotation delta: `\(deltaAngle)` crossed threshold `\(rotationThreshold)`")
-//            gestureType = .rotate
-//          } else if deltaPan.length > panThreshold {
-//            print("Zoom delta: `\(deltaZoom)` crossed threshold `\(zoomThreshold)`")
-//            gestureType = .pan
-//          }
-//        }
-        
         if gestureType == .none {
           let zoomPassed = deltaZoom > zoomThreshold
           let rotatePassed = deltaAngle > rotationThreshold
           let panPassed = deltaPan.length > panThreshold
-          
+
           /// Priority: pan > zoom > rotate
           if panPassed {
             gestureType = .pan
@@ -122,8 +108,6 @@ extension GestureStateHandler {
             break  // Wait for threshold to be crossed
         }
 
-      //        pan = lastPan + deltaPan
-
       case .ended, .cancelled, .none:
         lastPan = pan
         lastZoom = zoom
@@ -132,46 +116,5 @@ extension GestureStateHandler {
         currentTouchPositions = nil
         gestureType = .none
     }
-
-
   }
-
-
-  //
-  //  var pan = PanGestureState()
-  //  var zoom = ZoomGestureState()
-  //  var rotation = RotateGestureState()
-  //  var drawing = DrawingGestureState()
-
-  /// The space to which touch points are mapped (e.g., canvas or viewport)
-  //  var mappingRect: CGRect = .zero
-
-  //  mutating func update(event: TouchEventData, in rect: CGRect) {
-  //    pan.update(event: event, in: rect)
-  //    zoom.update(event: event, in: rect)
-  //    //    rotation.update(event: event, in: rect)
-  //    //    drawing.update(event: event, in: rect)
-  //  }
-
-  /// Something worth considering — a default cascade of gesture types?
-  /// Though they *can* be simultaneous, so maybe this isn't the way to go.
-  ///
-  /// Though could maintain a `Set<ActiveGesture>`?
-  ///
-  /// 1. Drawing as default
-  /// 2. If not drawing then Panning (e.g. *two* fingers?
-  ///   But what if wanting to *draw* with two fingers?). Can set a tool for that in the UI.
-  /// 3. If touches pinch and spread, then Zooming
-  /// 4. If
-  //  func determineActiveGestures(event: TouchEventData) -> Set<GestureType> {
-
-  /// What are the rules?
-  ///
-  /// Drawing: By default, only one touch. With a tool selected, multiple touches allowed
-  /// Pan: 2x touches. The default/defacto two-finger gesture type. Should be easiest to invoke
-  /// Zoom: 2x touches. Detected when finger distance changes sufficiently (past threshold)
-  /// Rotate: 2x touches. Similar to above, but change in rotation between the two touches
-
-
-  //  }
 }
