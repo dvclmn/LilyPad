69cc3ae912811e759f722d55f76dae6c1643dc07
Dave Coleman <dave@dvclmn.com>
2025-06-04 18:19:20 +0200

    Removes two large chunks of code from...
    ... GestureHandler

--- Sources/LilyPad/Handlers/Handler+Gesture.swift
+++ Sources/LilyPad/Handlers/Handler+Gesture.swift
@@ -24,14 +24,8 @@ public struct GestureStateHandler {
   public var initialTouchPair: TouchPair?
   public var lastTouchPair: TouchPair?
   public var currentTouchPair: TouchPair?
-  //  public var lastTouchPair: TouchPair?
 
   public var currentGestureType: GestureType = .none
-  //  var currentGestureID: TrackpadGesture.ID?
-  //  private var trackedTouchIDs: Set<TouchPoint.ID> = []
-
-
-  //  let requiredTouchCount: Int = 2
 
   public init() {}
 }
@@ -55,10 +49,6 @@ extension GestureStateHandler {
 
   public mutating func processGesture(touches: [MappedTouchPoint]) throws {
 
-//    print(
-//      "Processing touches, for GestureHandler. Initial Pair should be *the same* for every frame: \(self.initialTouchPair?.idsForComparison ?? "nil")"
-//    )
-    
     do {
       try recogniseGesture(touches: touches)
     
@@ -166,97 +156,6 @@ extension GestureStateHandler {
     }
   }
   
-//  private mutating func recogniseGesture(touches: [MappedTouchPoint]) throws {
-//    
-//    /// First, always validate and update the current touch pair
-//    guard
-//      let currentPair = TouchPair(
-//        touches,
-//        referencePair: self.currentTouchPair
-//      )
-//    else {
-//      self.currentGestureType = .none
-//      self.initialTouchPair = nil
-//      throw GestureError.touchesNotEqualToTwo
-//    }
-//    
-//    // Always update lastTouchPair before updating currentTouchPair
-//    self.lastTouchPair = self.currentTouchPair
-//    self.currentTouchPair = currentPair
-//    
-//    // Handle gesture recognition
-//    if self.currentGestureType == .none {
-//      // No gesture in progress - try to recognize one
-//      if let initialPair = self.initialTouchPair {
-//        // We have an initial pair, so determine gesture type
-//        let gestureType = GestureType(
-//          currentTouchPair: currentPair,
-//          initialPair: initialPair
-//        )
-//        self.currentGestureType = gestureType
-//      } else {
-//        // First frame - set initial pair
-//        self.initialTouchPair = currentPair
-//      }
-//    } else {
-//      // Gesture already in progress - validate we still have 2 touches
-//      if touches.count != 2 {
-//        self.currentGestureType = .none
-//        self.initialTouchPair = nil
-//        self.lastTouchPair = nil
-//        throw GestureError.touchesNotEqualToTwo
-//      }
-//      // Otherwise, just continue with the existing gesture
-//      // (currentTouchPair and lastTouchPair are already updated above)
-//    }
-//    
-////    guard self.currentGestureType == .none else {
-////      /// There is already a Gesture in progress
-////      if touches.count != 2 {
-////        self.currentGestureType = .none
-////        self.initialTouchPair = nil
-////        throw GestureError.touchesNotEqualToTwo
-////      } else {
-////        return
-////      }
-////    }
-////    
-////    /// Providing the current touch pair here in the TouchPair init
-////    /// in an attempt to keep the gesture touches stable across lifecycle
-////    guard
-////      let currentPair = TouchPair(
-////        touches,
-////        referencePair: self.currentTouchPair
-////      )
-////    else {
-////      self.currentGestureType = .none
-////      self.initialTouchPair = nil
-////      throw GestureError.touchesNotEqualToTwo
-////      
-////    }
-////    
-////    self.currentTouchPair = currentPair
-////    
-////    /// Let's check, do we have an initial (first) pair, as a reference point
-////    /// to calculate deltas, to identify gesture type?
-////    if let initialPair = self.initialTouchPair {
-////      
-////      /// If so, let's use `GestureType`s initialiser to get the type
-////      let gestureType = GestureType(
-////        currentTouchPair: currentPair,
-////        initialPair: initialPair
-////      )
-////      self.currentGestureType = gestureType
-////    } else {
-////      /// We only update the initial pair on the very first pass,
-////      /// whereas `lastTouchPair`, as below, is updated every frame
-////      initialTouchPair = currentPair
-////    }
-////    
-////    lastTouchPair = currentPair
-//    
-//  }
-
   public mutating func resetValue(for gestureType: GestureType) {
     switch gestureType {
       case .pan:
@@ -269,121 +168,5 @@ extension GestureStateHandler {
         break
     }
   }
-
-  //  public mutating func update(with touches: [MappedTouchPoint]) -> RawGesture? {
-  //    /// Exiting early right up top to reduce noise
-  //    guard touches.count > 1 else { return nil }
-  //
-  //    let activeTouches: [MappedTouchPoint] = touches.filter { $0.phase != .ended && $0.phase != .cancelled }
-  //    let activeTouchIDs = Set(activeTouches.map(\.id))
-  //
-  //    print(
-  //      "Currently \(activeTouches.count) active touches. Touches with Phases `ended` or `cancelled` are being ignored.")
-  //
-  //    /// Start a new gesture if eligible (only if not already tracking touches)
-  //    if activeTouchIDs.count == 2 && trackedTouchIDs.isEmpty {
-  //
-  //      print(
-  //        "Confirmed that there are exactly 2 active touches, and no existing tracked touch IDs. Starting new gesture")
-  //
-  //      /// Set the current gesture id to a new unique ID
-  //      let newGestureID = UUID()
-  //      currentGestureID = newGestureID
-  //
-  //      /// Update the active Touch IDs to be tracked
-  //      trackedTouchIDs = activeTouchIDs
-  //
-  //      return RawGesture(
-  //        id: newGestureID,
-  //        //        phase: .began,
-  //        touches: activeTouches
-  //      )
-  //    }
-  //
-  //    print(
-  //      "Cannot create a *new* gesture. Number of touches must be 2 (currently \(activeTouchIDs.count)), or there are touches already being tracked: \(trackedTouchIDs)."
-  //    )
-  //
-  //    /// Continue gesture if touches match
-  //    if activeTouchIDs == trackedTouchIDs {
-  //
-  //      print("The active touch IDs match the tracked touch IDs. Continuing the current \(currentGestureType) gesture.")
-  //      guard let currentGestureID else {
-  //        print("Couldn't get the `currentGestureID`")
-  //        return nil
-  //      }
-  //      return RawGesture(
-  //        id: currentGestureID,
-  //        //        phase: .changed,
-  //        touches: activeTouches
-  //      )
-  //    }
-  //
-  //    print("The active touch IDs do not match the tracked touch IDs. What happens next?")
-  //
-  //    /// End the gesture if one or more tracked touches ended
-  //    if !activeTouchIDs.isSuperset(of: trackedTouchIDs) {
-  //
-  //      print(
-  //        "Have determined that the active touch IDs are not a superset of the tracked touch IDs. Ending the current gesture."
-  //      )
-  //
-  //      guard let currentGestureID else {
-  //        print("Couldn't get the `currentGestureID` blarp")
-  //        return nil
-  //      }
-  //      let gesture = RawGesture(
-  //        id: currentGestureID,
-  //        //        phase: .ended,
-  //        touches: Array(activeTouches)
-  //      )
-  //      resetGestures()
-  //
-  //      return gesture
-  //    }
-  //
-  //    /// Default case: nothing to report
-  //    return nil
-  //  }
-
-  //  private func canTrackNewGesture(
-  //    activeTouchIDs: Set<MappedTouchPoint.ID>
-  //  ) -> Bool {
-  //    return activeTouchIDs.count == 2 && trackedTouchIDs.isEmpty
-  //  }
-  //
-  //
-  //  private mutating func resetGestures() {
-  //    currentGestureID = nil
-  //    trackedTouchIDs = []
-  //
-  //    print("Gestures were reset")
-  //  }
-
-  //  public func interpretGesture(
-  //    _ rawGesture: RawGesture,
-  //    lastTouchPair: TouchPair?
-  //  ) -> GestureType {
-  //    guard let touchPair = TouchPair(rawGesture.touches) else {
-  //      return .none
-  //    }
-  //
-  //    guard let lastTouchPair else {
-  //      return .none
-  //    }
-  //
-  //
-  //
-  //    /// Priority: pan > zoom > rotate
-  //    if panPassed {
-  //      return .pan
-  //    } else if zoomPassed {
-  //      return .zoom
-  //    } else if rotatePassed {
-  //      return .rotate
-  //    }
-  //
-  //    return .none
-  //  }
 
 }
