13efe0c826f6035ba1404014a37d5def8fb1ddd6
Dave Coleman <dave@dvclmn.com>
2025-06-04 18:16:19 +0200

    Updates

--- Sources/LilyPad/Handlers/Handler+Gesture.swift
+++ Sources/LilyPad/Handlers/Handler+Gesture.swift
@@ -124,54 +124,138 @@ extension GestureStateHandler {
     
   }
   
+
   private mutating func recogniseGesture(touches: [MappedTouchPoint]) throws {
     
-    guard self.currentGestureType == .none else {
-      /// There is already a Gesture in progress
-      if touches.count != 2 {
-        self.currentGestureType = .none
-        self.initialTouchPair = nil
-        throw GestureError.touchesNotEqualToTwo
-      } else {
-        return
-      }
+    /// Validate touch count
+    guard touches.count == 2 else {
+      self.currentGestureType = .none
+      self.initialTouchPair = nil
+      self.lastTouchPair = nil
+      throw GestureError.touchesNotEqualToTwo
     }
     
-    /// Providing the current touch pair here in the TouchPair init
-    /// in an attempt to keep the gesture touches stable across lifecycle
-    guard
-      let currentPair = TouchPair(
-        touches,
-        referencePair: self.currentTouchPair
-      )
-    else {
+    /// Create current touch pair
+    guard let currentPair = TouchPair(touches, referencePair: self.currentTouchPair) else {
       self.currentGestureType = .none
       self.initialTouchPair = nil
       throw GestureError.touchesNotEqualToTwo
-      
     }
     
+    /// Always update touch pair tracking
+    self.lastTouchPair = self.currentTouchPair
     self.currentTouchPair = currentPair
     
-    /// Let's check, do we have an initial (first) pair, as a reference point
-    /// to calculate deltas, to identify gesture type?
-    if let initialPair = self.initialTouchPair {
-      
-      /// If so, let's use `GestureType`s initialiser to get the type
-      let gestureType = GestureType(
-        currentTouchPair: currentPair,
-        initialPair: initialPair
-      )
-      self.currentGestureType = gestureType
-    } else {
-      /// We only update the initial pair on the very first pass,
-      /// whereas `lastTouchPair`, as below, is updated every frame
-      initialTouchPair = currentPair
+    /// Handle gesture state
+    switch self.currentGestureType {
+      case .none:
+        /// Try to recognize a new gesture
+        if let initialPair = self.initialTouchPair {
+          let gestureType = GestureType(
+            currentTouchPair: currentPair,
+            initialPair: initialPair
+          )
+          self.currentGestureType = gestureType
+        } else {
+          self.initialTouchPair = currentPair
+        }
+        
+      default:
+        /// Gesture in progress - just continue (touch pairs already updated)
+        break
     }
-    
-    lastTouchPair = currentPair
-    
   }
+  
+//  private mutating func recogniseGesture(touches: [MappedTouchPoint]) throws {
+//    
+//    /// First, always validate and update the current touch pair
+//    guard
+//      let currentPair = TouchPair(
+//        touches,
+//        referencePair: self.currentTouchPair
+//      )
+//    else {
+//      self.currentGestureType = .none
+//      self.initialTouchPair = nil
+//      throw GestureError.touchesNotEqualToTwo
+//    }
+//    
+//    // Always update lastTouchPair before updating currentTouchPair
+//    self.lastTouchPair = self.currentTouchPair
+//    self.currentTouchPair = currentPair
+//    
+//    // Handle gesture recognition
+//    if self.currentGestureType == .none {
+//      // No gesture in progress - try to recognize one
+//      if let initialPair = self.initialTouchPair {
+//        // We have an initial pair, so determine gesture type
+//        let gestureType = GestureType(
+//          currentTouchPair: currentPair,
+//          initialPair: initialPair
+//        )
+//        self.currentGestureType = gestureType
+//      } else {
+//        // First frame - set initial pair
+//        self.initialTouchPair = currentPair
+//      }
+//    } else {
+//      // Gesture already in progress - validate we still have 2 touches
+//      if touches.count != 2 {
+//        self.currentGestureType = .none
+//        self.initialTouchPair = nil
+//        self.lastTouchPair = nil
+//        throw GestureError.touchesNotEqualToTwo
+//      }
+//      // Otherwise, just continue with the existing gesture
+//      // (currentTouchPair and lastTouchPair are already updated above)
+//    }
+//    
+////    guard self.currentGestureType == .none else {
+////      /// There is already a Gesture in progress
+////      if touches.count != 2 {
+////        self.currentGestureType = .none
+////        self.initialTouchPair = nil
+////        throw GestureError.touchesNotEqualToTwo
+////      } else {
+////        return
+////      }
+////    }
+////    
+////    /// Providing the current touch pair here in the TouchPair init
+////    /// in an attempt to keep the gesture touches stable across lifecycle
+////    guard
+////      let currentPair = TouchPair(
+////        touches,
+////        referencePair: self.currentTouchPair
+////      )
+////    else {
+////      self.currentGestureType = .none
+////      self.initialTouchPair = nil
+////      throw GestureError.touchesNotEqualToTwo
+////      
+////    }
+////    
+////    self.currentTouchPair = currentPair
+////    
+////    /// Let's check, do we have an initial (first) pair, as a reference point
+////    /// to calculate deltas, to identify gesture type?
+////    if let initialPair = self.initialTouchPair {
+////      
+////      /// If so, let's use `GestureType`s initialiser to get the type
+////      let gestureType = GestureType(
+////        currentTouchPair: currentPair,
+////        initialPair: initialPair
+////      )
+////      self.currentGestureType = gestureType
+////    } else {
+////      /// We only update the initial pair on the very first pass,
+////      /// whereas `lastTouchPair`, as below, is updated every frame
+////      initialTouchPair = currentPair
+////    }
+////    
+////    lastTouchPair = currentPair
+//    
+//  }
 
   public mutating func resetValue(for gestureType: GestureType) {
     switch gestureType {
