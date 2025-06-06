7fa5906dadc189d0b3300122686dac30086c7ddf
Dave Coleman <dave@dvclmn.com>
2025-06-06 20:46:53 +0200

    Removes `shouldUseVelocity` from the View...
    ... modifier for now, as this should be controlled with a slider, not a toggle

--- Sources/LilyPad/Views/TouchesViewModifier.swift
+++ Sources/LilyPad/Views/TouchesViewModifier.swift
@@ -14,7 +14,7 @@ public typealias TouchesModifierOutput = ([MappedTouchPoint]) -> Void
 public struct TrackpadTouchesModifier: ViewModifier {
   @State private var localMappedTouches: [MappedTouchPoint] = []
 
-  let shouldUseVelocity: Bool
+//  let shouldUseVelocity: Bool
   let shouldShowIndicators: Bool
   let mappingRect: CGRect
   let touchUpdates: TouchesModifierOutput
@@ -39,7 +39,7 @@ public struct TrackpadTouchesModifier: ViewModifier {
       .drawingGroup()
 
       TrackpadTouchesView(
-        shouldUseVelocity: shouldUseVelocity
+        shouldUseVelocity: true
       ) { eventData in
 
         let touches = eventData?.touches ?? []
@@ -66,7 +66,7 @@ extension View {
   /// It's used in this modifier for the touch indicators only
   public func touches(
     //    isClickEnabled: Bool,
-    shouldUseVelocity: Bool = true,
+//    shouldUseVelocity: Bool = true,
     showIndicators: Bool = true,
     mappedTo mappingRect: CGRect,
     touchUpdates: @escaping TouchesModifierOutput
@@ -74,7 +74,7 @@ extension View {
     self.modifier(
       TrackpadTouchesModifier(
         //        isClickEnabled: isClickEnabled,
-        shouldUseVelocity: shouldUseVelocity,
+//        shouldUseVelocity: shouldUseVelocity,
         shouldShowIndicators: showIndicators,
         mappingRect: mappingRect,
         touchUpdates: touchUpdates
@@ -121,34 +121,34 @@ struct TrackpadShapeGuide: View {
 }
 
 
-public struct TouchesModifierExampleView: View {
-
-  public var body: some View {
-
-    GeometryReader { proxy in
-      Text("Hello")
-        .touches(
-          shouldUseVelocity: true,
-          showIndicators: true,
-          mappedTo: mappingRect(proxy.size)
-        ) { eventData in
-          //
-        }
-    }
-    .background(.black.secondary)
-
-  }
-}
-extension TouchesModifierExampleView {
-  func mappingRect(_ containerSize: CGSize) -> CGRect {
-    let exampleSize: CGSize = .init(width: 400, height: 300)
-    let rect: CGRect = exampleSize.toCGRect(in: containerSize)
-    return rect
-  }
-}
-#if DEBUG
-@available(macOS 15, iOS 18, *)
-#Preview(traits: .size(.normal)) {
-  TouchesModifierExampleView()
-}
-#endif
+//public struct TouchesModifierExampleView: View {
+//
+//  public var body: some View {
+//
+//    GeometryReader { proxy in
+//      Text("Hello")
+//        .touches(
+//          shouldUseVelocity: true,
+//          showIndicators: true,
+//          mappedTo: mappingRect(proxy.size)
+//        ) { eventData in
+//          //
+//        }
+//    }
+//    .background(.black.secondary)
+//
+//  }
+//}
+//extension TouchesModifierExampleView {
+//  func mappingRect(_ containerSize: CGSize) -> CGRect {
+//    let exampleSize: CGSize = .init(width: 400, height: 300)
+//    let rect: CGRect = exampleSize.toCGRect(in: containerSize)
+//    return rect
+//  }
+//}
+//#if DEBUG
+//@available(macOS 15, iOS 18, *)
+//#Preview(traits: .size(.normal)) {
+//  TouchesModifierExampleView()
+//}
+//#endif
