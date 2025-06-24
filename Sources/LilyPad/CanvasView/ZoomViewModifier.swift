f5918b63f4f9faebe1c315187533c5eb907a23a4
Dave Coleman <dave@dvclmn.com>
2025-06-24 13:33:24 +0200

    Updates

--- Sources/LilyPad/CanvasView/ZoomViewModifier.swift
+++ Sources/LilyPad/CanvasView/ZoomViewModifier.swift
@@ -5,22 +5,39 @@
 //  Created by Dave Coleman on 24/6/2025.
 //
 
-import SwiftUI
 import BaseHelpers
+import SwiftUI
 
 public struct ZoomViewModifier: ViewModifier {
-  
+
   @State private var ratioTracker: RatioTracker
   @GestureState private var magnification: CGFloat = 1
   @GestureState private var zoomGestureAnchor: UnitPoint = .center
-  
+
+  @Binding var zoom: CGFloat
+  @Binding var pan: CGPoint
+
+  public init(
+    zoom: Binding<CGFloat>,
+    pan: Binding<CGPoint>,
+    zoomRange: ClosedRange<Double>,
+  ) {
+    self._zoom = zoom
+    self._pan = pan
+    
+    let tracker = RatioTracker(range: zoomRange)
+    self._ratioTracker = State(initialValue: tracker)
+  }
+
   public func body(content: Content) -> some View {
-    content
-      .simultaneousGesture(zoomGesture(), isEnabled: true)
+    GeometryReader { proxy in
+      content
+        .simultaneousGesture(zoomGesture(in: proxy.size), isEnabled: true)
+    }
   }
 }
 extension ZoomViewModifier {
-  func zoomGesture() -> some Gesture {
+  func zoomGesture(in size: CGSize) -> some Gesture {
     MagnifyGesture()
       .updating($magnification) { value, state, _ in
         state = value.magnification
@@ -30,57 +47,46 @@ extension ZoomViewModifier {
       }
       .onChanged { value in
         let scale = ratioTracker.ratio(from: value.magnification)
-        let newZoom = store.gestureHandler.zoom * scale
-        
+        let newZoom = zoom * scale
+
         /// Convert startAnchor to absolute view coordinates
         let anchorPoint = CGPoint(
-          x: store.canvasViewportSize.width * zoomGestureAnchor.x,
-          y: store.canvasViewportSize.height * zoomGestureAnchor.y)
-        
+          x: size.width * zoomGestureAnchor.x,
+          y: size.height * zoomGestureAnchor.y)
+
         /// Position of anchor in canvas space before zooming
         let anchorCanvasPositionBefore = CGPoint(
-          x: (anchorPoint.x - store.gestureHandler.pan.x) / store.gestureHandler.zoom,
-          y: (anchorPoint.y - store.gestureHandler.pan.y) / store.gestureHandler.zoom
+          x: (anchorPoint.x - pan.x) / zoom,
+          y: (anchorPoint.y - pan.y) / zoom
         )
-        
+
         /// New pan that keeps that canvas point under anchorPoint
         let newPan = CGPoint(
           x: anchorPoint.x - anchorCanvasPositionBefore.x * newZoom,
           y: anchorPoint.y - anchorCanvasPositionBefore.y * newZoom
         )
-        
-        store.gestureHandler.zoom = newZoom
-        store.gestureHandler.pan = newPan
+
+        zoom = newZoom
+        pan = newPan
       }
       .onEnded { _ in
         ratioTracker.reset()
-        store.gestureHandler.zoom = store.gestureHandler.zoom
-        store.gestureHandler.pan = store.gestureHandler.pan
       }
-    
-    //      .updating($magnification) { value, state, _ in
-    //        value.startAnchor
-    ////        print(
-    ////          "`updating` Magnification: \(value.magnification.displayString). Start Anchor: \(value.startAnchor.displayString). Start location: \(value.startLocation.displayString)."
-    ////        )
-    //        state = value.magnification
-    //      }
-    //      .onChanged { value in
-    //        let ratio = ratioTracker.ratio(from: value.magnification)
-    ////        totalZoom *= ratio
-    //        store.gestureHandler.zoom *= ratio
-    ////        print(
-    ////          "`onChanged` ratio: \(ratio.displayString). `store.gestureHandler.zoom`: \(store.gestureHandler.zoom.displayString)"
-    ////        )
-    //      }
-    //      .onEnded { _ in
-    //        ratioTracker.reset()
-    //      }
   }
 
 }
 extension View {
-  public func example() -> some View {
-    self.modifier(ZoomViewModifier())
+  public func zoomView(
+    zoom: Binding<CGFloat>,
+    pan: Binding<CGPoint>,
+    zoomRange: ClosedRange<Double>,
+  ) -> some View {
+    self.modifier(
+      ZoomViewModifier(
+        zoom: zoom,
+        pan: pan,
+        zoomRange: zoomRange
+      )
+    )
   }
 }
