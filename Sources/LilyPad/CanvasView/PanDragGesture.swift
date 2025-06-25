5ba3ac484fd2eba814ca964238a29929afa9640d
Dave Coleman <dave@dvclmn.com>
2025-06-25 08:27:44 +0200

    Trying out moving CanvasView to BaseComponents...
    ... so I can use some of the visual things in there

--- Sources/LilyPad/CanvasView/PanDragGesture.swift
+++ Sources/LilyPad/CanvasView/PanDragGesture.swift
@@ -8,46 +8,35 @@
 import SwiftUI
 
 public typealias DragChanged = (_ gestureValue: DragGesture.Value, _ initial: CGPoint) -> Void
-
 public typealias DragEnded = (_ gestureValue: DragGesture.Value, _ initial: CGPoint) -> CGPoint
-//public typealias DragEnded = (_ gestureValue: DragGesture.Value, _ initial: inout CGPoint) -> Void
 
 public struct DragGestureModifier: ViewModifier {
-  
+
   @State private var initialDragLocation: CGPoint = .zero
   @GestureState private var dragOffset: CGSize = .zero
-  //  @State private var didStartDrag: Bool = false
-  
+
   let isEnabled: Bool
   let onDragChanged: DragChanged
   let onDragEnded: DragEnded
-  
+
   public func body(content: Content) -> some View {
     content
       .simultaneousGesture(makeConditionalDragGesture, isEnabled: isEnabled)
   }
 }
 extension DragGestureModifier {
-  
+
   var makeConditionalDragGesture: some Gesture {
     DragGesture(minimumDistance: 0)
       .updating($dragOffset) { value, state, _ in
-        //        guard isEnabled else { return }
         state = value.translation
       }
       .onChanged { value in
-        //        guard isEnabled else { return }
         onDragChanged(value, initialDragLocation)
       }
       .onEnded { value in
-        //        guard isEnabled else { return }
         initialDragLocation = onDragEnded(value, initialDragLocation)
       }
-    //      .onEnded { value in
-    //        guard isEnabled else { return }
-    //        var mutableInitial = initialDragLocation
-    //        onDragEnded(value, &mutableInitial)
-    //      }
   }
 }
 extension View {
